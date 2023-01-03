8c779f8ca6cc4ef775876f2a3e89c0194c434670
AlexBecalick <alexander.becalick@crick.ac.uk>
2023-01-03 20:01:29 +0000

    Fix typo in cellData

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -3,7 +3,7 @@ function config() {
             roi: {"x0": 0, "x1": 56730, "y0": 0, "y1": 33150 },
             zoomLevels: 10, // maximum zoom levels. Leave that at 10.
             tiles: 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg_ZZZ',
-            cellData: [{mediaLink: 'https://media.githubusercontent.com/media.com/AlexBecalick/alexbecalick.github.io/main/viewer/data/cellData.tsv?raw=true', size: "77390949"}],
+            cellData: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/alexbecalick.github.io/main/viewer/data/cellData.tsv?raw=true', size: "77390949"}],
             geneData: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/alexbecalick.github.io/main/viewer/data/geneData.tsv?raw=true', size: "640650110"}],
             cellBoundaries: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/alexbecalick.github.io/main/viewer/data/cellBoundaries.tsv?raw=true', size: "69547441"}],
             }

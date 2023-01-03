36cc952a5c6330445601eb6f4bd09ba70291bd34
AlexBecalick <alexander.becalick@crick.ac.uk>
2023-01-03 18:19:41 +0000

    Fixed mediaLink to data

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -4,7 +4,7 @@ function config() {
             zoomLevels: 10, // maximum zoom levels. Leave that at 10.
             tiles: 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg_ZZZ',
             cellData: [{mediaLink: 'https://github.com/AlexBecalick/alexbecalick.github.io/blob/main/viewer/data/cellData.tsv?raw=true', size: "77390949"}],
-            geneData: [{mediaLink: 'https://github.com/AlexBecalick/alexbecalick.github.io/blob/main/viewer/data/geneData.tsv', size: "640650110"}],
-            cellBoundaries: [{mediaLink: 'https://github.com/AlexBecalick/alexbecalick.github.io/blob/main/viewer/data/cellBoundaries.tsv', size: "69547441"}],
+            geneData: [{mediaLink: 'https://github.com/AlexBecalick/alexbecalick.github.io/blob/main/viewer/data/geneData.tsv?raw=true', size: "640650110"}],
+            cellBoundaries: [{mediaLink: 'https://github.com/AlexBecalick/alexbecalick.github.io/blob/main/viewer/data/cellBoundaries.tsv?raw=true', size: "69547441"}],
         }
 }

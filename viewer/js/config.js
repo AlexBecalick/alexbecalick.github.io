9045a5f95c45e1b31b85910ab5783fb6d61888e8
AlexBecalick <alexander.becalick@crick.ac.uk>
2023-01-03 19:55:22 +0000

    CORS request fixed by changing data url. In future use api.github.com as in acyclic MsBrain

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -3,8 +3,8 @@ function config() {
             roi: {"x0": 0, "x1": 56730, "y0": 0, "y1": 33150 },
             zoomLevels: 10, // maximum zoom levels. Leave that at 10.
             tiles: 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg_ZZZ',
-            cellData: [{mediaLink: './data/cellData.tsv', size: "77390949"}],
-            geneData: [{mediaLink: './data/geneData.tsv', size: "640650110"}],
-            cellBoundaries: [{mediaLink: './data/cellBoundaries.tsv', size: "69547441"}],
+            cellData: [{mediaLink: 'https://media.githubusercontent.com/media.com/AlexBecalick/alexbecalick.github.io/main/viewer/data/cellData.tsv?raw=true', size: "77390949"}],
+            geneData: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/alexbecalick.github.io/main/viewer/data/geneData.tsv?raw=true', size: "640650110"}],
+            cellBoundaries: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/alexbecalick.github.io/main/viewer/data/cellBoundaries.tsv?raw=true', size: "69547441"}],
             }
         }
